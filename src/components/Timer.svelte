76eafc73563614e4b53d42660f51e33436414c9a
DarkDev <darkdev_brand@riseup.net>
2022-12-29 00:20:46 +0200

    add countdown timer

---
+++ src/components/Timer.svelte
@@ -0,0 +1,30 @@
+<div class="mb-10 text-center font-impact text-light border rounded-lg border-light max-w-2xl p-4 h-auto">
+    <h2 class="w-full text-center text-light font-impact lg:text-5xl md:text-4xl text-4xl max-w-3xl ">
+        Następny turniej odbędzie się za
+    </h2>
+  <div id="countdown">
+    <ul>
+      <li><span id="days"></span>Dni</li>
+      <li><span id="hours"></span>Godziny</li>
+      <li><span id="minutes"></span>Minuty</li>
+      <li><span id="seconds"></span>Sekundy</li>
+    </ul>
+  </div>
+</div>
+
+
+<style>
+li {
+  display: inline-block;
+  font-size: 1.5em;
+  list-style-type: none;
+  padding: 1em;
+  text-transform: uppercase;
+}
+
+li span {
+  display: block;
+  font-size: 4.5rem;
+}
+
+</style>
