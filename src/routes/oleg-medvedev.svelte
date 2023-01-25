863288b6c5da908fc3609c4dc10dbff82a28c381
DarkDev <darkdev_brand@riseup.net>
2023-01-25 02:18:54 +0200

    alfa v. organisator page

---
+++ src/routes/oleg-medvedev.svelte
@@ -0,0 +1,186 @@
+<script>
+
+
+</script>
+
+<svelte:head>
+	<title>Oleg Medvedev</title>
+</svelte:head>
+
+<main class="bg-gray-100"> 
+    <div class="flex justify-start w-full p-10 ">
+        <h1 style="margin-left: 18%;" class="text-center bg-light  max-w-2xl rounded-lg p-10 text-dark font-impact lg:text-5xl md:text-4xl text-4xl ">
+            Informacje o organizatorze turnieju «Żółty słoń»
+        </h1>
+    </div>
+
+    <section class="w-full h-auto flex flex-col justify-center ">
+        <div class="flex w-full flex-row justify-center">
+            <div class="flex flex-col w-4/12 bg-light p-6 rounded-lg items-center cards-wrap z-10">
+                <div>
+                    <span class="text-dark font-impact text-4xl">Cześć, nazywam się</span>
+                    <h2 class="w-full text-center text-dark font-impact text-7xl max-w-3xl ">
+                        Oleg Medvedev
+                    </h2>
+                </div>
+            <div  
+                data-aos="fade-up"
+                data-aos-delay="100"
+                data-aos-duration="1000" 
+                data-aos-once="false"
+                data-aos-mirror="true" 
+                style="background-image: url(/organisator/champion.svg);" class="card-term items-end flex">
+                    <div style="height: 200px; width: 360px;" class="bg-dark text-light rounded-lg border border-light shadow-md flex max-w-sm flex-col lg:p-4 md:p-4 p-2">
+                        <span class="text-8xl font-impact tracking-tight text-white text-center">
+                            Mistrz
+                        </span> 
+                        <span class="text-4xl -mt-2 font-impact tracking-tight text-white text-center">
+                            Białorusi
+                        </span> 
+                    </div>
+                </div>
+            <div 
+                data-aos="fade-down"
+                data-aos-delay="100"
+                data-aos-duration="1000" 
+                data-aos-once="false"
+                data-aos-mirror="true" 
+                style="background-image: url(/organisator/ranking.svg); " class="max-w-sm card-ranking items-center flex">
+                    <div style="height: 180px; width: 360px;" class="bg-dark text-light rounded-lg border border-light shadow-md flex max-w-sm flex-col lg:p-4 md:p-4 p-2 ">
+                        <span class="text-4xl font-impact tracking-tight text-white text-center">
+                            Nagrod turniejowuch 
+                        </span> 
+                        <span class="text-8xl font-impact tracking-tight text-white text-center">
+                            38
+                        </span> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="-ml-40 flex photo-wrap items-start"> 
+                <img 
+                    data-aos="zoom-in"
+                    data-aos-delay="100"
+                    data-aos-duration="1000" 
+                    data-aos-once="false"
+                    data-aos-mirror="true" 
+                    src="./organisator/avatar_champion.png" class="photo-card" alt="Photo organisator">
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="w-full -mt-40 h-auto flex flex-col justify-center ">
+        <div class="flex w-full flex-row justify-center">
+            <div class="-mr-40 flex photo-wrap items-end"> 
+                <img 
+                    data-aos="zoom-in"
+                    data-aos-delay="100"
+                    data-aos-duration="1000" 
+                    data-aos-once="false"
+                    data-aos-mirror="true" 
+                    src="./organisator/avatar_bussines.png" class="photo-card" alt="Photo organisator">
+            </div>
+
+                <div class="flex flex-col w-4/12 bg-light p-6 rounded-lg items-center cards-wrap z-10">
+                                    <div 
+                data-aos="fade-down"
+                data-aos-delay="100"
+                data-aos-duration="1000" 
+                data-aos-once="false"
+                data-aos-mirror="true" 
+                style="background-image: url(/terms/terms_ranking.svg); " class="max-w-sm card-ranking items-center flex">
+                    <div style="height: 180px; width: 360px;" class="bg-dark text-light rounded-lg border border-light shadow-md flex max-w-sm flex-col lg:p-4 md:p-4 p-2 ">
+
+                        <span class="text-4xl font-impact tracking-tight text-white text-center">
+                            Rankinging FIDE
+                        </span> 
+                        <span class="text-8xl font-impact tracking-tight text-white text-center">
+                            1954
+                        </span> 
+                    </div>
+                </div>
+
+                <div  
+                data-aos="fade-up"
+                data-aos-delay="100"
+                data-aos-duration="1000" 
+                data-aos-once="false"
+                data-aos-mirror="true" 
+                style="background-image: url(/terms/terms_clocks.svg);" class="card-term items-end flex">
+                    <div style="height: 200px; width: 360px;" class="bg-dark text-light rounded-lg border border-light shadow-md flex max-w-sm flex-col lg:p-4 md:p-4 p-2">
+                        <p class="flex flex-row justify-center">
+                            <span class="text-8xl font-impact tracking-tight text-white text-center">
+                                8 
+                            </span> 
+                            <span class="pl-6 text-8xl font-impact tracking-tight text-white text-center">
+                                lat
+                            </span> 
+                        </p>
+                        
+                        <span class="text-4xl font-impact tracking-tight text-white text-center">
+                            kariery sportowej
+                        </span> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <section class="w-full flex justify-center items-center flex-row pt-12 pb-12">
+        <div style="height: 400px;" class="flex w-8/12 flex-col bg-light rounded-lg p-8 z-0">
+            <h2 class="w-full text-dark font-impact text-7xl max-w-3xl ">
+                Kontakt
+            </h2>
+            <p class="text-dark text-4xl m-1 max-w-4xl">
+                Zapraszam na indywidualne i grupowe zajęcia szachowe.
+            </p>
+
+            <div class="flex flex-row w-full">
+                <a href="https://www.facebook.com/hulio.mondre">
+                    <img alt="facebook_profile" src="./social/org_contact_facebook.svg" 
+                    class="rounded-full w-40 h-40 m-1  ">
+                </a>
+                <a href="https://www.facebook.com/hulio.mondre">
+                    <img alt="github_profile" src="./social/org_contact_github.svg" 
+                    class="rounded-full w-40 h-40 m-1  ">
+                </a>
+                <a href="https://www.facebook.com/hulio.mondre">
+                    <img alt="linkedin_profile" src="./social/org_contact_linkedin.svg" 
+                    class="rounded-full w-40 h-40 m-1  ">
+                </a>
+
+            </div>
+        </div>
+
+        <div class="-ml-60 flex mini-photo-wrap items-end z-10"> 
+            <img 
+                data-aos="zoom-in"
+                data-aos-delay="100"
+                data-aos-duration="1000" 
+                data-aos-once="false"
+                data-aos-mirror="true" 
+                src="./organisator/avatar_dream.png" class="photo-card" alt="Photo organisator">
+        </div>
+    </section>
+</main>
+
+
+
+<style>
+    .photo-card {
+        max-height: 900px;
+        z-index: 0;
+    }
+
+    .photo-wrap {
+        height: 130vh;
+    }
+    .mini-photo-wrap {
+        width: 600px;
+    }
+</style>
