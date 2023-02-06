75f1c75c7891042872ae5194ad22c4ec336b7249
DarkDev <darkdev_brand@riseup.net>
2023-02-06 23:41:29 +0200

    add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,88 @@
+<footer class="bg-dark">
+	<div class="flex flex-row justify-center py-8 px-6">
+		<div style="width: 340px;">
+			<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase">Mój profil IT</h2>
+			<ul class="text-gray-300">
+				<li class="mb-4">
+					<a href="https://darkdev.vercel.app/" class="hover:underline" target="_blank">Resume </a>
+				</li>
+				<li class="mb-4">
+					<a href="https://github.com/oleg-darkdev/" class="hover:underline" target="_blank"
+						>Github
+					</a>
+				</li>
+				<li class="mb-4">
+					<a
+						href="https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F"
+						class="hover:underline"
+						>LinkedIn
+					</a>
+				</li>
+				<!-- <li class="mb-4">
+					<a href="/" target="_blank" class="hover:underline">Brand Center </a>
+				</li>
+				<li class="mb-4">
+					<a href="/" target="_blank" class="hover:underline">Blog </a>
+				</li> -->
+			</ul>
+		</div>
+		<div style="width: 340px;">
+			<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Moje projekty społeczne</h2>
+			<ul class="text-gray-300">
+				<li class="mb-4">
+					<a href="https://yellow-elephant.vercel.app/chess" target="_blank" class="hover:underline"
+						>Turniej szachowy: Żółty Słoń</a
+					>
+				</li>
+				<li class="mb-4">
+					<a
+						href="https://yellow-elephant.vercel.app/checkers"
+						target="_blank"
+						class="hover:underline"
+						>Turniej warcabowy: Żółty Pionek
+					</a>
+				</li>
+				<li class="mb-4">
+					<a href="https://itingo.vercel.app/" class="hover:underline" target="_blank"
+						>IT wsparcie dla wspierających</a
+					>
+				</li>
+				<li class="mb-4">
+					<a href="https://44fablab.vercel.app/" class="hover:underline" target="_blank"
+						>44FabLab w Sopocie</a
+					>
+				</li>
+				<li class="mb-4">
+					<a href="https://44games.vercel.app/" class="hover:underline" target="_blank"
+						>DarkDev.Games</a
+					>
+				</li>
+			</ul>
+		</div>
+		<div style="width: 340px;">
+			<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Kontakt</h2>
+			<ul class="text-gray-300">
+				<li class="mb-4">
+					<a href="https://www.facebook.com/hulio.mondre" class="hover:underline" target="_blank"
+						>Facebook
+					</a>
+				</li>
+				<li class="mb-4">
+					<a
+						target="_blank"
+						href="https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F"
+						class="hover:underline"
+						>LinkedIn
+					</a>
+				</li>
+				<li class="mb-4">
+					<a target="_blank" href="mailto:oleg@darkdev.games" class="hover:underline">Email </a>
+				</li>
+				<!-- 
+				<li class="mb-4">
+					<a href="/" target="_blank" class="hover:underline">MacOS </a>
+				</li> -->
+			</ul>
+		</div>
+	</div>
+</footer>
