fd0f7588fe4a99378033f7160756214591de0f52
DarkDev <darkdev_brand@riseup.net>
2023-02-07 17:27:57 +0200

    alfa v.

---
+++ src/data/chessTurnamentsList.js
@@ -0,0 +1,100 @@
+const tournamentList = {
+	title: 'Turnieje szachowe',
+	partners: [
+		{
+			logo: '/partners/partner_44_cubes.svg',
+			link: '',
+			title: '44 cubes'
+		},
+		{
+			logo: '/partners/partner_darkdev.svg',
+			link: '',
+			title: 'DarkDev progect'
+		},
+		{
+			logo: '/partners/partner_itingo.svg',
+			link: '',
+			title: 'ITingo'
+		},
+		{
+			logo: '/partners/partner_elephant.svg',
+			link: '',
+			title: 'Yellow Elephant'
+		}
+	],
+	schedule: [
+		{
+			date: '17.11.2022',
+			linkFb: 'https://www.facebook.com/events/3222760484644932',
+			linkTg: '',
+			addres: `Mazowiecka 26, Sopot`,
+			img: 'https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-6/312622489_1324585104612954_3132663826861128700_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=340051&_nc_ohc=WsEjQc2LJJ8AX8IKJOI&_nc_ht=scontent-frt3-1.xx&oh=00_AfCslML0jJGwWmQHW56v1tELm8o7OkJGB8UuHyWcAWSCUQ&oe=63AB009F',
+			title: 'Turnieje szachowy "Żółty słoń" #1'
+		},
+		{
+			date: '01.03.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #2'
+		},
+		{
+			date: '05.04.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #3'
+		},
+		{
+			date: '03.05.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #4'
+		},
+		{
+			date: '07.06.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #5'
+		},
+		{
+			date: '05.07.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #6'
+		},
+		{
+			date: '09.08.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #7'
+		},
+		{
+			date: '06.09.2023',
+			linkFb: '',
+			linkTg: '',
+			addres: `"Sopoteka" - T. Kościuszki 14, 81-704, Sopot, Polska`,
+
+			img: '',
+			title: 'Turnieje szachowy "Żółty słoń" #7'
+		}
+	]
+};
+
+export default tournamentList;
