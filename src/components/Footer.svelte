bb64d4a6042c7ea44ccf39edeb585234a91e5158
DarkDev <darkdev_brand@riseup.net>
2023-02-07 05:50:58 +0200

    fix align

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,36 +1,36 @@
 <footer class="bg-dark">
 	<div class="flex flex-row justify-center py-8 px-6">
-		<div style="width: 340px;">
+		<div style="width: 150px;">
 			<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase">Mój profil IT</h2>
 			<ul class="text-gray-300">
 				<li class="mb-4">
-					<a href="https://darkdev.vercel.app/" class="hover:underline" target="_blank">Resume </a>
+					<a href="https://darkdev.vercel.app/" class="hover:underline; hover:text-light" target="_blank">Resume </a>
 				</li>
 				<li class="mb-4">
-					<a href="https://github.com/oleg-darkdev/" class="hover:underline" target="_blank"
+					<a href="https://github.com/oleg-darkdev/" class="hover:underline; hover:text-light" target="_blank"
 						>Github
 					</a>
 				</li>
 				<li class="mb-4">
 					<a
 						href="https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F"
-						class="hover:underline"
+						class="hover:underline; hover:text-light"
 						>LinkedIn
 					</a>
 				</li>
 				<!-- <li class="mb-4">
-					<a href="/" target="_blank" class="hover:underline">Brand Center </a>
+					<a href="/" target="_blank" class="hover:underline; hover:text-light">Brand Center </a>
 				</li>
 				<li class="mb-4">
-					<a href="/" target="_blank" class="hover:underline">Blog </a>
+					<a href="/" target="_blank" class="hover:underline; hover:text-light">Blog </a>
 				</li> -->
 			</ul>
 		</div>
-		<div style="width: 340px;">
+		<div style="width: 300px;">
 			<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Moje projekty społeczne</h2>
 			<ul class="text-gray-300">
 				<li class="mb-4">
-					<a href="https://yellow-elephant.vercel.app/chess" target="_blank" class="hover:underline"
+					<a href="https://yellow-elephant.vercel.app/chess" target="_blank" class="hover:underline; hover:text-light"
 						>Turniej szachowy: Żółty Słoń</a
 					>
 				</li>
@@ -38,32 +38,32 @@
 					<a
 						href="https://yellow-elephant.vercel.app/checkers"
 						target="_blank"
-						class="hover:underline"
+						class="hover:underline; hover:text-light"
 						>Turniej warcabowy: Żółty Pionek
 					</a>
 				</li>
 				<li class="mb-4">
-					<a href="https://itingo.vercel.app/" class="hover:underline" target="_blank"
+					<a href="https://itingo.vercel.app/" class="hover:underline; hover:text-light" target="_blank"
 						>IT wsparcie dla wspierających</a
 					>
 				</li>
 				<li class="mb-4">
-					<a href="https://44fablab.vercel.app/" class="hover:underline" target="_blank"
+					<a href="https://44fablab.vercel.app/" class="hover:underline; hover:text-light" target="_blank"
 						>44FabLab w Sopocie</a
 					>
 				</li>
 				<li class="mb-4">
-					<a href="https://44games.vercel.app/" class="hover:underline" target="_blank"
+					<a href="https://44games.vercel.app/" class="hover:underline; hover:text-light" target="_blank"
 						>DarkDev.Games</a
 					>
 				</li>
 			</ul>
 		</div>
-		<div style="width: 340px;">
+		<div style="width: 150px;">
 			<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Kontakt</h2>
 			<ul class="text-gray-300">
 				<li class="mb-4">
-					<a href="https://www.facebook.com/hulio.mondre" class="hover:underline" target="_blank"
+					<a href="https://www.facebook.com/hulio.mondre" class="hover:underline; hover:text-light" target="_blank"
 						>Facebook
 					</a>
 				</li>
@@ -71,16 +71,16 @@
 					<a
 						target="_blank"
 						href="https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F"
-						class="hover:underline"
+						class="hover:underline; hover:text-light"
 						>LinkedIn
 					</a>
 				</li>
 				<li class="mb-4">
-					<a target="_blank" href="mailto:oleg@darkdev.games" class="hover:underline">Email </a>
+					<a target="_blank" href="mailto:oleg@darkdev.games" class="hover:underline; hover:text-light">Email </a>
 				</li>
 				<!-- 
 				<li class="mb-4">
-					<a href="/" target="_blank" class="hover:underline">MacOS </a>
+					<a href="/" target="_blank" class="hover:underline; hover:text-light">MacOS </a>
 				</li> -->
 			</ul>
 		</div>
