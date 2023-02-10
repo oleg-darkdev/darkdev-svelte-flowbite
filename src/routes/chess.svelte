e263be0168722a079f4d2117fd1a8a511d7eae17
DarkDev <darkdev_brand@riseup.net>
2023-02-11 00:31:54 +0200

    refactoring data

--- src/routes/chess.svelte
+++ src/routes/chess.svelte
@@ -8,6 +8,9 @@
 	import ListTournaments from '../components/tournaments/ListTournaments.svelte';
 	import tournamentData from '../data/chessTurnamentsList';
 	import DescriptionChess from '../components/DescriptionChess.svelte';
+	import sponsors from '../data/sponsors';
+	import organisators from '../data/organisators';
+	import socialMedia from '../data/socialMedia';
 
 	let firstColumnData = [
 		'Turniej warcabowy: Żółty Pionek',
@@ -82,60 +85,6 @@
 		]
 	];
 
-	const organisators = [
-		// {
-		// 	logo: '/partners/partner_sopot.svg',
-		// 	link: 'https://www.sopot.pl/',
-		// 	title: 'Sopot miasto'
-		// },
-		{
-			logo: '/partners/partner_44_cubes.svg',
-			link: '',
-			title: '44 cubes'
-		},
-
-		{
-			logo: '/partners/partner_sopoteka.svg',
-			link: 'https://www.mbp.sopot.pl/placowki/sopoteka',
-			title: 'Sopoteka'
-		}
-	];
-
-	const sponsors = [
-		{
-			logo: '/partners/partner_darkdev.svg',
-			link: '',
-			title: 'DarkDev progect'
-		},
-		{
-			logo: '/partners/partner_itingo.svg',
-			link: 'https://itingo.vercel.app/',
-			title: 'ITingo'
-		}
-	];
-
-	const socialMedia = [
-		{
-			link: 'https://www.facebook.com/yellow.elephant.tournament/',
-			img: './social/facebook.svg',
-			title: 'Facebook'
-		},
-		// {
-		//     link: '',
-		//     img: './social/instagram.svg',
-		//     title: ''
-		// },
-		{
-			link: 'https://t.me/chess_yellow_elephant',
-			img: './social/telegram.svg',
-			title: 'Telegram'
-		},
-		{
-			link: '',
-			img: './social/linkedin.svg',
-			title: 'Linkedin'
-		}
-	];
 
 	onMount(() => {
 		startTimer();
