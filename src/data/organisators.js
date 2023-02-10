56df4fe6723879e26654a04799a47645aacc3d02
DarkDev <darkdev_brand@riseup.net>
2023-02-11 00:31:22 +0200

    refactoring data

---
+++ src/data/organisators.js
@@ -0,0 +1,21 @@
+const organisators = [
+	// {
+	// 	logo: '/partners/partner_sopot.svg',
+	// 	link: 'https://www.sopot.pl/',
+	// 	title: 'Sopot miasto'
+	// },
+	{
+		logo: '/partners/partner_44_cubes.svg',
+		link: '',
+		title: '44 cubes'
+	},
+
+	{
+		logo: '/partners/partner_sopoteka.svg',
+		link: 'https://www.mbp.sopot.pl/placowki/sopoteka',
+		title: 'Sopoteka'
+	}
+];
+
+
+export default organisators;
---
+++ src/data/sponsors.js
@@ -0,0 +1,15 @@
+const sponsors = [
+	{
+		logo: '/partners/partner_darkdev.svg',
+		link: '',
+		title: 'DarkDev progect'
+	},
+	{
+		logo: '/partners/partner_itingo.svg',
+		link: 'https://itingo.vercel.app/',
+		title: 'ITingo'
+	}
+];
+
+
+export default sponsors;
